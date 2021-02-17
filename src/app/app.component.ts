529f1dfe468efbcf9a8188087cb87a68107353b7
Travis Mix <travismix1980@gmail.com>
2021-02-16 18:44:14 -0800

    Added all premade pipes we are going to cover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onAmountChange(amount: string): void{
+    this.amount = parseFloat(amount);
+  }
+
+  onDateChange(date: string): void{
+    this.date = date;
+  }
+
+  onNameChange(name: string): void {
+    this.name = name;
+  }
 }
